5b77fb466017335d9dee5c548e5bdd8ac6b1c167
Luis Cobian <coby10@gmail.com>
2016-05-19 10:50:37 -0500

    Ejecucion de Java en android Studio
    http://luiscobian.blogspot.mx/2016/05/java-ejecutar-aplicacionees-java-en.html

---
+++ javaruntime/src/main/java/com/example/Ejemplo.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import javax.swing.JOptionPane;
+
+public class Ejemplo {
+
+    public static void main(String ...a){
+        System.out.println("Hola Mundo");
+        JOptionPane.showMessageDialog(null,"Hola");
+    }
+}
